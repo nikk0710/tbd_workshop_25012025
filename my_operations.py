09fa10edcdb21e0b544b829fdf3f0498dd26d524
nikk0710 <aphirak_k@cmu.ac.th>
2025-01-25 14:10:35 +0700

    Up

--- my_operations.py
+++ my_operations.py
@@ -1,14 +1,14 @@
 # Member A: edit this function.
 def add(num1: int, num2: int) -> None:
-    # Edit code under this line
-    
+
+    res = num1 + num2
+
     print(num1, "+", num2, "is", res)
     pass
 
 
 # Member B: edit this function.
 def diff(num1: int, num2: int) -> None:
-    # Edit code under this line
     res = num1 - num2
     print(num1, "-", num2, "is", res)
     pass
@@ -16,7 +16,6 @@ def diff(num1: int, num2: int) -> None:
 
 # Member C: edit this function.
 def mul(num1: int, num2: int) -> None:
-    # Edit code under this line
-
     print(num1, "*", num2, "is", res)
     pass
+
